4aec4c92460ebb1450e268c8175d2ecfbb236d27
ahmed kamel <skownx9@gmail.com>
2024-06-12 14:08:47 +0300

    add global error handle and route error handle

--- app.js
+++ app.js
@@ -31,7 +31,18 @@ app.use(express.static(path.resolve(__dirname, "public")));
 
 //mount routes
 app.use("/api/v1/tasks", taskRoute);
+// handle routes error
 
+app.get("*", (req, res, next) => {
+	const err = new Error(`cant find this route ${req.originalUrl}`);
+	next(err.message);
+});
+
+// global handle error
+app.use((err, req, res, next) => {
+	res.status(500).json({ error: err });
+	next();
+});
 // start server
 const serverPort = process.env.PORT || 3000;
 app.listen(3000, () => {
